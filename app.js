1af83e76743bc31ef5e12f0219f8b56508dd9726
xixilive <xixilive@gmail.com>
2013-11-27 19:35:01 +0800

    WIP

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 
-var app = express(), image = require('./image');
+var app = express();
+var image = require('./image');
 
 // all environments
 app.set('port', process.env.PORT || 3004);
@@ -15,12 +16,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use(image());
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(function(req, res, next){
-  image.createImage(req, res, next);
-});
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
--- image.js
+++ image.js
@@ -1,21 +1,17 @@
 var Canvas = require('canvas'), 
     Image = Canvas.Image,
+    Font = Canvas.Font,
     fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    crypto = require('crypto');
 
 var HotImage = function(config){
-  config = config || {}
   this.config = HotImage.defaultConfig;
-  for(var k in config){
-    if(config[k]){
-      this.config[k] = config[k]
-    }
-  }
+  this.configure(config);
   this.loadThemes();
 };
 
 HotImage.defaultConfig = {
-  cache: true,
   route_prefix: 'g',
   theme_root: __dirname + '/images/',
   named_colors: {
@@ -24,7 +20,20 @@ HotImage.defaultConfig = {
     blue: '#0000FF'
   },
   default_colors: ["#DDD", "#666"],
-  max_size: 1200
+  max_size: 1920,
+  fonts: {
+    'angelina': __dirname + '/fonts/' + 'angelina.ttf'
+  },
+  jpg_quantity: 75
+};
+
+HotImage.prototype.configure = function(config){
+  if(typeof config == 'object'){
+    for(var k in config){
+      this.config[k] = config[k];
+    }
+  }
+  return this;
 };
 
 HotImage.prototype.loadThemes = function(){
@@ -32,10 +41,16 @@ HotImage.prototype.loadThemes = function(){
     return this.themes;
   }
 
-  var d = this.config.theme_root, themes = {};
+  var d = this.config.theme_root, themes = {}, sta;
   fs.readdirSync(d).forEach(function(fn){
     if(fs.statSync(path.join(d, fn)).isDirectory()){
-      themes[fn.toLowerCase()] = fs.readdirSync(path.join(d, fn)).map(function(f){ return path.join(d, fn, f); });
+      themes[fn.toLowerCase()] = fs.readdirSync(path.join(d, fn)).map(function(f){
+        f = path.join(d, fn, f);
+        return {
+          file: f,
+          mtime: fs.statSync(f).mtime.getTime()
+        };
+      });
     }
   });
 
@@ -54,7 +69,7 @@ HotImage.prototype.parseOptions = function(url){
     return null;
   }
 
-  var options = {theme: null, colors: null};
+  var options = {theme: null, colors: null, font: 'angelina'};
   options.size = match[1].split(/x/i).map(function(s){ return Math.max(parseInt(s) || 1, 1); });
   options.ext = (match[6] || "png").toLowerCase().replace('e','');
 
@@ -81,6 +96,16 @@ HotImage.prototype.parseOptions = function(url){
     options.colors = config.default_colors;
   }
 
+  var mt = "", f;
+  if(options.theme){
+    f = this.randomImageFile(options.theme);
+    options.theme_file = f.file;
+    mt = f.mtime;
+  }
+
+  options.cache_id = [options.size.join("x"), (options.colors || []).join(','), options.theme_file, mt].join("-").toLowerCase();
+  options.cache_id = crypto.createHash('md5').update(options.cache_id, 'utf8').digest('hex');
+  
   return options;
 };
 
@@ -91,9 +116,13 @@ HotImage.prototype.randomImageFile = function(theme){
 
 HotImage.prototype.createImage = function(req, res, next){
   var options = this.parseOptions(req.url);
-  // console.log(options)
   if(!options){
     return next();
+  }
+
+  if(req.get('If-None-Match') == options.cache_id){
+    res.status(304).end();
+    return;
   }
 
   var canvas = new Canvas(options.size[0], options.size[1]),
@@ -106,17 +135,39 @@ HotImage.prototype.createImage = function(req, res, next){
     context.fillRect(0, 0, options.size[0], options.size[1]);
     var img = new Image;
     img.onload = function(){
-      // TODO: crop image
-      // var w = img.width, h = img.height;
-      // if(w < )
-      context.drawImage(img, 0, 0, options.size[0], options.size[1]);//, 0, 0, options.size[0], options.size[1]);
+      var x = 0, y = 0, w, h, rs = img.width / img.height, rd = options.size[0] / options.size[1];
+      if(rs <= 1){
+        w = options.size[0];
+        h = (w / img.width) * img.height;
+        y = (h - options.size[1]) / -2;
+        if( h < options.size[1]){
+          h = options.size[1];
+          w = (h / img.height) * img.width;
+          x = (w - options.size[0]) / -2;
+          y = 0;
+        }
+      }else{
+        h = options.size[1];
+        w = (h / img.height) * img.width;
+        x = (w - options.size[0]) / -2;
+        if(w < options.size[0]){
+          w = options.size[0];
+          h = (w / img.width) * img.height;
+          x = 0;
+          y = (h - options.size[1]) / -2;
+        }
+      }
+      context.drawImage(img, x, y, w, h);
     };
-    // console.log(this.randomImageFile(options.theme));
-    img.src = this.randomImageFile(options.theme);
+    img.src = options.theme_file;
     context.save();
   }else{
+    var font_size = [[14, 0, 100], [18, 101, 200], [24, 201, 500], [32, 501, 800], [40, 801, 10000]].filter(function(r, i){
+      return r[1] <= options.size[0] && r[2] >= options.size[0];
+    })[0][0] || 14;
+    context.addFont(new Font(options.font, this.config.fonts[options.font]));
     context.antialias = 'gray';
-    context.font = '14px Arial';
+    context.font = font_size + 'px ' + options.font;
     context.textBaseline = "middle";
     var text_rect = context.measureText(text);
     context.fillStyle = options.colors[0];
@@ -129,7 +180,7 @@ HotImage.prototype.createImage = function(req, res, next){
   switch(options.ext){
     case 'jpg':
       stream = canvas.createJPEGStream({
-        quality: 75
+        quality: this.config.jpg_quantity
       });
       break;
 
@@ -139,17 +190,33 @@ HotImage.prototype.createImage = function(req, res, next){
   }
 
   if(stream){
+    res.setHeader('Content-Type', 'image/' + options.ext);
+    var exp = new Date(), age = 86400*1;
+    res.setHeader('Content-Control', 'public, max-age=' + age);
+    exp.setTime(exp.getTime() + age*1000);
+    res.setHeader('Expires', exp.toUTCString());
+    res.setHeader('ETag', options.cache_id);
     stream.pipe(res, function(err){
       if(err){
         return next(err);
       }
-      //TODO:: cache response
-      res.header('Content-Type', 'image/' + options.ext);
       res.end();
-    })
-    return;
+    });
+  }else{
+    next();
   }
-
 };
 
-module.exports = new HotImage({max_size: 1600});
+var hotimage = new HotImage();
+
+module.exports = function(config){
+  hotimage.configure(config);
+
+  return function(req, res, next){
+    if(req.method != 'GET'){
+      res.status(405).end();
+      return;
+    }
+    hotimage.createImage(req, res, next);
+  }
+};
